253ea04673622cb702dfd3b62de9f49e90d01801
vignesh-prakash <vignesh24p@gmail.com>
2024-09-24 20:36:34 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
 
-// TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
-//     int expectedresult = 0;
-//     const char* input = "Hello, world!";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
+TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
+    int expectedresult = 0;
+    const char* input = "Hello, world!";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
 
 TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
@@ -15,30 +15,30 @@ TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     ASSERT_EQ(result, expectedresult);
 }
 
-// TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
-//     int expectedresult = 3;
-//     const char*  input = "1,2";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
+TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
+    int expectedresult = 3;
+    const char*  input = "1,2";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
 
-// TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
-//     int expectedresult = 6;
-//     const char*  input = "1\n2,3";
-//     int result =add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
+TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
+    int expectedresult = 6;
+    const char*  input = "1\n2,3";
+    int result =add(input);
+    ASSERT_EQ(result, expectedresult);
+}
 
-// TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
-//     int expectedresult = 1;
-//     const char*  input = "1,1001";
-//     int result =add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
+TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
+    int expectedresult = 1;
+    const char*  input = "1,1001";
+    int result =add(input);
+    ASSERT_EQ(result, expectedresult);
+}
 
-// TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
-//     int expectedresult = 3;
-//     const char*  input = "//;\n1;2";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+    int expectedresult = 3;
+    const char*  input = "//;\n1;2";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
